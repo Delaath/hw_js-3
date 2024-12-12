dac16497159620628611b2f4f8d43d63a1c7283f
Delaath <mr.mok367@gmail.com>
2024-12-12 17:53:24 +0200

    first push

---
+++ main.js
@@ -0,0 +1,16 @@
+const result = 5 + 5 + "5";
+console.log(result);
+
+const email = "zakopaylo18@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+const firstWord = "My";
+const secondWord = "name";
+const thirsdWord = "is"
+const fullName = firstWord + " " + secondWord + " " + thirsdWord + " " + "Viktor";
+console.log(fullName);
+
+const userName = prompt("Введіть ваше ім'я");
+const payment = 250;
+alert("Дякуємо " + userName + "! " + "До сплати " + payment + " гривень");
